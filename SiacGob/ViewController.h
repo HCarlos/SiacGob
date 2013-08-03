5dd102ebc707fe7c76983bdf66f2a1b31757e1ae
Carlos H <manager@logydes.com>
2013-08-03 18:03:14 -0500

    Initial Commit

---
+++ SiacGob/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SiacGob
+//
+//  Created by DevCH on 03/08/13.
+//  Copyright (c) 2013 DevCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SiacGob/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SiacGob
+//
+//  Created by DevCH on 03/08/13.
+//  Copyright (c) 2013 DevCH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
